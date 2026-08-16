8620b281fe2d57cfcbb67abe972a15152b8f681e
wedhaka <wedaka@gmail.com>
2026-08-16 03:29:14 +0200

    ex 05 complted

--- 05_lemmatization.py
+++ 05_lemmatization.py
@@ -2,4 +2,16 @@ import spacy
 
 nlp = spacy.load("en_core_web_sm")
 
-doc = nlp(u"Tesla is looking at buying U.S. Startup for $6 million")
+doc = nlp(u"I am a runner running in a race because I love to run since i ran today")
+
+# for token in doc : 
+    # print(token.text, '\t', token.pos_, token.lemma, '\t', token.lemma_)
+
+def show_lemmas(text) :
+    for token in text : 
+        print(f'{token.text:{12}} {token.pos_:{6}} {token.lemma:<{22}} {token.lemma_}')
+
+
+doc2 = nlp(u"I saw ten mice today!")
+
+show_lemmas(doc2)
