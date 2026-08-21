c176eaf750773744cf83487d8de2ccb09f6fa78d
wedhaka <wedaka@gmail.com>
2026-08-21 22:40:47 +0200

    modify the changes

--- nlp_basics/11_ex.py
+++ nlp_basics/11_ex.py
@@ -3,13 +3,3 @@ import spacy
 nlp = spacy.load("en_core_web_sm")
 doc = nlp(u"Tesla is looking at buying U.S. Startup for $6 million")
 
-
-for token in doc:
-    print(token.text, token.pos_, token.dep_)
-
-# print(nlp.pipeline)
-
-doc2[0].pos_
-doc2[0].dep_
-
-doc3 = nlp(u"")
